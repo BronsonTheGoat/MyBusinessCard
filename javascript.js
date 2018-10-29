cd51be46223a6a970fb59dddbf1bf15c01803b7d
Sebestyen Zoltan <zsebest14@gmail.com>
2018-10-29 20:04:24 +0100

    Clearing css
    Avatar and name position

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
     $("#btn1").click(function(){
         $(".about-me").hide();
         $(".business-card").animate({width: '0vw', height: '0vh'},"slow");
-        $(".contact").animate({width: '100vw', height: '100vh'});
+        $(".contact").animate({width: '100vw', height: '100vh'},"slow");
         $(".contact").show(500);
     });
   $("#btn2").click(function(){
         $(".contact").hide(200);
         $(".contact").animate({width: '0vw', height: '0vh'},"slow");
-        $(".business-card").animate({width: '100vw', height: '100vh'}, "slow");
+        $(".business-card").animate({width: '100vw', height: '100vh'},"slow");
         $(".about-me").show();
     });
 });
